ba202d76cf786175ec2f5f538801ada6ed0bf78d
Cecile Tonglet <cecile.tonglet@cecton.com>
2026-05-19 21:46:12 +0200

    feat(xtask): skip default command when -s or -w flags are provided

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -13,11 +13,17 @@ fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
     let opt: Opt = clap::Parser::parse();
     match opt {
-        Opt::Watch(watch) => watch.run({
-            let mut run_command = Command::new("cargo");
-            run_command.args(["run", "--release", "--package", "explorer"]);
-            run_command
-        }),
+        Opt::Watch(watch) => {
+            if watch.shell_commands.is_empty() && watch.cargo_commands.is_empty() {
+                watch.run({
+                    let mut run_command = Command::new("cargo");
+                    run_command.args(["run", "--release", "--package", "explorer"]);
+                    run_command
+                })
+            } else {
+                watch.run([])
+            }
+        }
         Opt::UpdateThemes => update_themes::run(),
     }
 }
